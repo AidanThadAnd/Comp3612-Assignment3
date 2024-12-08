93bb4f4864da0fcf9f11c8ba3131c4303f622ac1
AidanThadAnd <aidanandrews112@gmail.com>
2024-12-07 23:17:46 -0700

    Added example API request

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
+# COMP 3612 - F1 API Project
+## Overview
+This repository holds the code for an F1 API which aims to provide endpoints for the following data:
+1. Circuits
+2. Constructors
+3. Drivers
+4. Races
+5. Results
+
+As of December 7th, 2024 the API is currently hosted on glitch.com. All requests will be returned with the JSON data or an applicable JSON error.
+
+## Technologies used
+- Node.js
+- Express.js
+
+
+## TESTING
 - https://star-pattern-appalachiosaurus.glitch.me/api/circuits
 - https://star-pattern-appalachiosaurus.glitch.me/api/circuits/1
 - https://star-pattern-appalachiosaurus.glitch.me/api/constructors
@@ -21,3 +38,14 @@
 - https://star-pattern-appalachiosaurus.glitch.me/api/results/race/1756348576
 - https://star-pattern-appalachiosaurus.glitch.me/api/results/season/2023
 - https://star-pattern-appalachiosaurus.glitch.me/api/results/season/2034
+
+### NOTE:
+- /api/constructors/javascript **WILL RETURN AN ERROR** due to invalid constructor "javascript"
+- /api/constructorResults/mclaren/2040 **WILL RETURN AN ERROR** due to the invalid year "2024"
+- /api/constructorResults/comp3612/2023 **WILL RETURN AN ERROR** due to invalid consturctorRef "comp3612"
+- /api/drivers/randy **WILL RETURN AN ERROR** due to invalid driver Reference "randy"
+- /api/driverResults/piastre/2002 **WILL RETURN AN ERROR** due to invalid name + year
+- /api/races/seasoning/2023 **WILL RETURN AN ERROR** due to invalid API endpoint "seasoning"
+- /api/races/season/2032 **WILL RETURN AN ERROR** due to invalid season "2032"
+- /api/results/race/1756348576 **WILL RETURN AN ERROR** due to invalid raceID "1756348576"
+- /api/results/season/2034 **WILL RETURN AN ERROR** due to invalid year "2034"
--- assign3/Node/tempCodeRunnerFile.js
+++ assign3/Node/tempCodeRunnerFile.js
@@ -6,8 +6,6 @@ const app = express()
 //Base case "/" returns a welcome message
 app.get("/api", (req,resp)=>{resp.send("Welcome to the Formula 1 API")})
 
-
-
 function readJson(fileName){
     const jsonPath = path.join("../", "data", fileName)
     const jsonData = fs.readFileSync(jsonPath, "utf8")
@@ -44,9 +42,9 @@ const constructors = readJson(constructorFileName)
 //handle request for all constructors
 app.get("/api/constructors", (req,resp)=>{resp.json(constructors)})
 //handle request for specific constructor
-app.get("/api/constructors/:constructorRef", (req,resp)=>{
-    const constructorRef = req.params.constructorRef.toLowerCase()
-    const foundConstructors=constructors.filter(c=>c.constructorRef==constructorRef)
+app.get("/api/constructors/:ref", (req,resp)=>{
+    const ref = req.params.ref
+    const foundConstructors=constructors.filter(c=>c.constructorRef=ref)
 
     if(foundConstructors.length==0)
         return resp.json({error: {message: "Value not found or Incorrect query string values"}})
@@ -65,9 +63,10 @@ const drivers = readJson(driverFileName)
 //handle request for all drivers
 app.get("/api/drivers", (req,resp)=>{resp.json(drivers)})
 //handle request for specific driver
-app.get("/api/drivers/:driverRef", (req,resp)=>{
-    const driverRef = req.params.driverRef.toLowerCase()
-    const foundDrivers=drivers.filter(d=>d.driverRef==driverRef)
+app.get("/api/drivers/:ref", (req,resp)=>{
+    const ref = req.params.ref
+    console.log(ref)
+    const foundDrivers=drivers.filter(d=>d.driverRef=ref)
 
     if(foundDrivers.length==0)
         return resp.json({error: {message: "Value not found or Incorrect query string values"}})
@@ -107,7 +106,7 @@ const results = readJson(resultsFileName)
 //ConstructorsResults for season
 app.get("/api/constructorResults/:constructorRef/:season", (req,resp)=>{
     const season = req.params.season
-    const constructorRef = req.params.constructorRef.toLowerCase()
+    const constructorRef = req.params.constructorRef
 
     const foundConstructorResults = results.filter(r=>(r.constructor.ref == constructorRef && r.race.year == season))
 
@@ -120,7 +119,7 @@ app.get("/api/constructorResults/:constructorRef/:season", (req,resp)=>{
 //DriverResults for season
 app.get("/api/driverResults/:driverRef/:season", (req,resp)=>{
     const season = req.params.season
-    const driverRef = req.params.driverRef.toLowerCase()
+    const driverRef = req.params.driverRef
 
     const foundDriverResults = results.filter(r=>(r.driver.ref == driverRef && r.race.year == season))
 
@@ -153,11 +152,6 @@ app.get("/api/results/season/:year", (req,resp)=>{
         return resp.json({error: {message: "Value not found or Incorrect query string values"}})
 
     resp.json(foundSeasonResults)
-})
-
-//Handle invalid API endpoints
-app.get("/api/*", (req, resp) => {
-    resp.json({ error: { message: "Error: not a valid API endpoint", api: req.originalUrl } })
 })
 
 
